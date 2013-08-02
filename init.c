c2d2bc13bc40a006ca61c90bef27953def610974
kevinfhell <kevin.gao.love@gmail.com>
2013-08-02 23:06:07 +0800

    another commit

--- init.c
+++ init.c
@@ -3,4 +3,9 @@
 void main(void)
 {
     printf("init module\n");
+    printf("init module\n");
+    printf("init module\n");
+    printf("init module\n");
+    printf("init module\n");
+    printf("init module\n");
 }
